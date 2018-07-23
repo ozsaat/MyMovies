851c75d491d81a6956b19903202e5006370341e0
Saat, Oz <osaat@gumtree.com>
2018-07-23 17:12:24 +0100

    Detail view

--- app/src/main/java/com/osaat/mymovies/DetailsActivity.kt
+++ app/src/main/java/com/osaat/mymovies/DetailsActivity.kt
@@ -2,6 +2,12 @@ package com.osaat.mymovies
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_details.*
+import kotlinx.android.synthetic.main.movie_row.view.*
+
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -9,12 +15,30 @@ class DetailsActivity : AppCompatActivity() {
         const val EXTRA_MOVIE = "movie_details"
     }
 
+    private lateinit var backdropImage: ImageView
+    private lateinit var overview: TextView
+    private lateinit var rating: TextView
+    private lateinit var releaseDate: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
-//        val bundle = intent.getBundleExtra(EXTRA_MOVIE)
-//        var movie  = bundle.getParcelable(EXTRA_MOVIE) as Movie
+        val intent = intent
+        val movie = intent.getParcelableExtra<Movie>(EXTRA_MOVIE)
+
+        println("Detail movie YYYYYYYYY" + movie)
+
+        backdropImage = movieBackDrop
+        releaseDate = movieReleaseDate
+        overview = movieOverview
+        rating = movieRating
+
+        Picasso.get().load("http://image.tmdb.org/t/p/w342/${movie.backdropPath}")
+                .into(backdropImage)
+        releaseDate.text = movie.releaseDate
+        rating.text = movie.voteAverage.toString()
+        overview.text = movie.overview
 
     }
 }
